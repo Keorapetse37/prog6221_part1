b48c002c62c41c93effd9e340c1e652fae4819bc
Keorapetse Tlhapane <st10479702@vcconnect.edu.za>
2026-03-31 10:01:51 +0200

    Implemented ChatbotUI with ASCII art and text formatting.

---
+++ CyberBot/CyberBot/ChatbotUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace CyberBot
+{
+    public class ChatbotUI
+    {
+        // Method for conversational typing effect
+        public static void TypeLine(string text, ConsoleColor color = ConsoleColor.White, int delay = 20)
+        {
+            Console.ForegroundColor = color;
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(delay);
+            }
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+
+        public static void DrawHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(@"
+   ______      __               ____        __ 
+  / ____/_  __/ /_  ___  ____  / __ )____  / /_
+ / /   / / / / __ \/ _ \/ ___// __  / __ \/ __/
+/ /___/ /_/ / /_/ /  __/ /   / /_/ / /_/ / /_  
+\____/\__, /_.___/\___/_/   /_____/\____/\__/  
+     /____/                                    
+            ");
+            Console.WriteLine(new string('=', 50));
+            Console.ResetColor();
+        }
+    }
+}
